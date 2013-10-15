b57dc7264ceeabf09d7ccd9dd089da8d8bf7c7b7
atrimm <alexander.trimm@gmail.com>
2013-10-15 14:03:05 -0400

    added vertex constructor

--- src/DataStructures/vertex.java
+++ src/DataStructures/vertex.java
@@ -5,8 +5,17 @@ public class vertex
 	private String loc;
 	public double xCoord;
 	public double yCoord;
+	public int ID;
 	private static enum locType {CITY, DUNGEON};
 	
+	public vertex(String loc, double xCoord, double yCoord, int ID)
+	{
+		this.loc = loc;
+		this.xCoord = xCoord;
+		this.yCoord = yCoord;
+		this.ID = ID;
+	}
+
 	public String loc()
 	{
 		return loc;
@@ -21,6 +30,6 @@ public class vertex
 	{
 		double xDist = v.xCoord-xCoord;
 		double yDist = v.yCoord-yCoord;		
-		return Math.sqrt(Math.pow(xDist,2)+Math.pow(yDist,2));
+		return Math.	sqrt(Math.pow(xDist,2)+Math.pow(yDist,2));
 	}
 }
